152cf28d65820573ccbf4ef22e6a7237276db15f
Ajith Anand <ajithanand303@gmail.com>
2023-08-22 19:22:27 +0530

    front banner dynamic loading

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import './Banner.css'
+import { useState,useEffect } from 'react'
+import axios  from '../../config/axios'
+import {API_KEY,IMAGE_URL}  from '../../constants/constant'
 
 
 function Banner() {
+    const [movie,setMovie] = useState({})
+    useEffect(()=>{
+        axios.get(`trending/all/week?api_key=${API_KEY}`).then((res)=>{
+            // console.log(res.data.results[0]);
+            setMovie(res.data.results[2])
+        });
+
+    },[])   
+
+
+
     return (
-        <div className='banner'>
+        <div style={{backgroundImage:`url("${IMAGE_URL + movie.backdrop_path}")`}} className='banner'>
             <div className='banner__contents'>
-                <h1 className='banner__title'>Movie Name</h1>
+                <h1 className='banner__title'>{movie ? movie.title : ''}</h1>
                 <div className='banner_buttons'>
                     <button className='button'>Play</button>
                     <button className='button'>My list</button>
                 </div>
-                <h1 className='banner__description'>Description :In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying</h1>
+                <h1 className='banner__description'>{movie.overview}</h1>
             </div>
 
             <div className="fade_bottom">
